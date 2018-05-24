0b4fa38203685d28844335372329a83325367ee3
dxiong <914622765@qq.com>
2018-05-24 15:37:31 +0800

    day3 动态路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+// 引入组件
+import Test from '@/view/test'  
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // }
+    // 路由参数填写
     {
-      path: '/',
+      path: '/test/:testId/name/:testName',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: Test
     }
   ]
 })
---
+++ src/view/test.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="test">
+		<!-- 获取路由ID -->
+		this is id: {{ $route.params.testId }}
+		<br>
+		this is name: {{ $route.params.testName }}
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				msg: "Hello Vue!"
+			}
+		}
+	}
+</script>
+<style scoped></style>
