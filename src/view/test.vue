3d9957e220172af41fbdabef34224438199c27e3
dxiong <914622765@qq.com>
2018-05-28 16:20:09 +0800

    day4_嵌套路由 day5_编程式路由

--- src/view/test.vue
+++ src/view/test.vue
@@ -1,9 +1,19 @@
 <template>
 	<div class="test">
+		<!-- 动态路由 -->
 		<!-- 获取路由ID -->
 		this is id: {{ $route.params.testId }}
 		<br>
 		this is name: {{ $route.params.testName }}
+		<br>
+
+		<!-- 嵌套路由 -->
+		<router-link to="/test/title1">标题1</router-link>
+		<router-link to="/test/title2">标题2</router-link>
+		<router-view></router-view>
+
+		<!-- 编程式路由 -->
+		<div @click="gotoGoods">跳转到商品详情页面</div>
 	</div>
 </template>
 <script>
@@ -12,6 +22,25 @@
 			return {
 				msg: "Hello Vue!"
 			}
+		},
+		methods: {
+			gotoGoods: function () {
+				// this.$router.push('/goods');
+				// 路由添加参数 方式一
+				/*this.$router.push({
+					path: '/goods?goodsId=666'
+				});*/
+				// 路由添加参数 方式二
+				this.$router.push({
+					path: 'goods',
+					query: {
+						goodsId: 667
+					}
+				});
+			}
+			/*gotoGoods () {
+				this.$router.push('/goods');
+			}*/
 		}
 	}
 </script>
---
+++ src/view/title2.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="title2">
+		这里是 title2 的内容！
+	</div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<style scoped></style>
